f081477f78ebca090a6fbf21e49f4a168895605c
fliegla <43549412+fliegla@users.noreply.github.com>
2022-10-17 15:28:53 +0200

    Update __init__.py

--- CustomInvertedPendulumModel/Model/__init__.py
+++ CustomInvertedPendulumModel/Model/__init__.py
@@ -2,6 +2,7 @@ from gym.envs.registration import register
 
 register(
   id='DiffDrive-v0',
-  entry_point='customEnvironments.envs:DiffDriveEnv',
+  entry_point='Model.envs:InvertedPendulumEnv',
   max_episode_steps=1000,
+  reward_threshold=950.0,
 )
